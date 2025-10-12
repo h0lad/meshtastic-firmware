e8bb377ef03a5364ceb417beec7b815df26d14da
Holger Adams <mail@dm5tt.de>
2025-10-12 15:28:32 +0200

    loraharvesterbox: add new device

---
+++ variants/stm32/loraharvesterbox/variant.h
@@ -0,0 +1,19 @@
+/*
+STM32WLE5 Core Module for LoRaWAN® RAK3372
+https://store.rakwireless.com/products/wisblock-core-module-rak3372
+*/
+
+/*
+This variant is a work in progress.
+Do not expect a working Meshtastic device with this target.
+*/
+
+#ifndef _VARIANT_LORAHARVESTERBOX_
+#define _VARIANT_LORAHARVESTERBOX_
+
+#define USE_STM32WLx
+
+#define LED_PIN PA8 // Green LED
+#define LED_STATE_ON 0
+
+#endif
